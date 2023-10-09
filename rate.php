afc717771d19ff1df3889d7c54ce07371bbe22d4
Izabella Gashi <118194798+callixst@users.noreply.github.com>
2023-10-09 20:46:07 +0200

    Update rate.php

--- rate.php
+++ rate.php
@@ -1,52 +1,41 @@
 <?php
 require_once "base.php";
 require_once "session.php";
-error_log("Received rating: " . $_POST['rating']);
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Preveri, ali so bili vsi potrebni podatki poslani
-    if (isset($_POST["rating"]) && isset($_POST["id_v"]) && isset($_POST["id_c"])) {
-        // Pridobi podatke iz POST zahtevka
-        $rating = intval($_POST["rating"]);
-        $id_v = intval($_POST["id_v"]);
-        $id_c = intval($_POST["id_c"]);
-
-        // Preveri veljavnost ocene (npr. med 1 in 5)
-        if ($rating < 1 || $rating > 5) {
-            http_response_code(400); // Napaka: Neveljavna ocena
-            echo json_encode(["error" => "Neveljavna ocena"]);
-            exit;
-        }
-
-
-        // Pripravi in izvedi SQL poizvedbo za vstavljanje ocene v tabelo rating
-        $stmt = $pdo->prepare("INSERT INTO ratings (rating, id_v, id_c) VALUES (:rating, :id_v, :id_c)");
-        $stmt->bindParam(':rating', $rating, PDO::PARAM_INT);
-        $stmt->bindParam(':id_v', $id_v, PDO::PARAM_INT);
-        $stmt->bindParam(':id_c', $id_c, PDO::PARAM_INT);
-
-        if ($stmt->execute()) {
-            http_response_code(200); // Success
-            echo json_encode(["message" => "Rating successfully inserted"]);
-        } else {
-            http_response_code(500); // Server error
-            echo json_encode(["error" => "Error inserting rating: " . $stmt->errorInfo()[2]]);
-        }
-
-
-        // Zapri povezavo do podatkovne baze
-        $stmt->close();
-        $pdo->close();
-    } else {
-        http_response_code(400); // Napaka: Manjkajoči podatki
-        echo json_encode(["error" => "Manjkajoči podatki"]);
-    }
-} else {
-    http_response_code(405); // Napaka: Neveljavna metoda
-    echo json_encode(["error" => "Neveljavna zahteva"]);
+if (!isset($_SESSION['user_id'])) {
+    // Preverite, ali je uporabnik prijavljen. Če ni, lahko dodate ustrezno obvladovanje napak.
+    echo "Uporabnik ni prijavljen.";
+    exit();
 }
 
-?>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Preberite oceno iz AJAX zahteve
+    $userRating = $_POST["rating"];
+    $videoId = $_POST["video_id"]; // Preberite tudi video_id iz AJAX zahteve
 
+    // Preverite, ali uporabnik že ocenil ta video
+    $userId = $_SESSION['user_id'];
+
+    $sqlCheckRating = "SELECT * FROM ratings WHERE id_v = ? AND id_c = ?";
+    $stmtCheckRating = $pdo->prepare($sqlCheckRating);
+    $stmtCheckRating->execute([$videoId, $userId]);
+
+    if ($stmtCheckRating->rowCount() > 0) {
+        // Uporabnik je že ocenil ta video, obravnavajte to situacijo po vaših željah.
+        echo "This user has already rated this videp.";
+    } else {
+        // Uporabnik še ni ocenil tega videa, shrani oceno v tabelo 'ratings'
+        $currentTime = date("Y-m-d H:i:s");
+
+        $sqlInsertRating = "INSERT INTO ratings (rating, time_r, id_v, id_c) VALUES (?, ?, ?, ?)";
+        $stmtInsertRating = $pdo->prepare($sqlInsertRating);
+        $stmtInsertRating->execute([$userRating, $currentTime, $videoId, $userId]);
+
+        // Vrnite odgovor na AJAX zahtevo
+        echo "Rating saved";
+    }
+} else {
+    // Neveljavna zahteva, lahko dodate dodatno obvladovanje napak tukaj
+    echo "Neveljavna zahteva.";
+}
+?>
